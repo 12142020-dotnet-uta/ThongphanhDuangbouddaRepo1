49c0ac3c0f9331895b4eae0b27a8752ed1a11270
Duangboudda <tongpan78@hotmail.com>
2020-12-16 11:55:10 -0800

    none

--- Program.cs
+++ Program.cs
@@ -15,26 +15,6 @@ namespace ThongphanhDuangbouddaRepo1
             bool parsResult = false;
             int userInputInt = 0;
             
-            do{
-
-                string userInput = Console.ReadLine();
-            //bool result = false;
-                //int userResponse = int.Parse(userInput);
-                parsResult = int.TryParse(userInput,out userInputInt);
-                if(parsResult == false){
-                    Console.WriteLine("Please Enter a number");
-                    continue;
-                }
-                if(userInputInt > 3 || userInputInt < 1){
-                    Console.WriteLine("Input is out of range");
-                    continue;
-                }
-                result = false;
-
-            }while(result);
-
-            Console.WriteLine("Congratulation you enter : " + userInputInt);
-
         }
     }
 }
