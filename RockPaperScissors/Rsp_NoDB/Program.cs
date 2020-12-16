3708e219161b93953d607eebc59fc159e20784f1
Duangboudda <tongpan78@hotmail.com>
2020-12-16 10:39:02 -0800

    add do while loop

--- RockPaperScissors/Rsp_NoDB/Program.cs
+++ RockPaperScissors/Rsp_NoDB/Program.cs
@@ -6,7 +6,30 @@ namespace Rsp_NoDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            bool result = true;
+            bool parsResult = false;
+            int userInputInt = 0;
+            
+            do{
+
+                string userInput = Console.ReadLine();
+            //bool result = false;
+                //int userResponse = int.Parse(userInput);
+                parsResult = int.TryParse(userInput,out userInputInt);
+                if(parsResult == false){
+                    Console.WriteLine("Please Enter a number");
+                    continue;
+                }
+                if(userInputInt > 3 || userInputInt < 1){
+                    Console.WriteLine("Input is out of range");
+                    continue;
+                }
+                result = false;
+
+            }while(result);
+
+            Console.WriteLine("Congratulation you enter : " + userInputInt);
         }
     }
 }
