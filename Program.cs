deb442be0c2f763ce63def5ae53e0e32f5923afc
Duangboudda <tongpan78@hotmail.com>
2020-12-16 08:31:24 -0800

    new sentence for console output

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ThongphanhDuangbouddaRepo1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I tyeped this Hello World not computer template :)");
+            
         }
     }
 }
