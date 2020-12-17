2936e9cd50c147a8010f96471841951abe54edce
Duangboudda <tongpan78@hotmail.com>
2020-12-16 21:57:02 -0800

    Added extention

--- RockPaperScissors/Rsp_NoDB/Program.cs
+++ RockPaperScissors/Rsp_NoDB/Program.cs
@@ -7,6 +7,8 @@ namespace Rsp_NoDB
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+
+
             bool result = true;
             bool parsResult = false;
             int userInputInt = 0;
@@ -20,35 +22,45 @@ namespace Rsp_NoDB
                 //int userResponse = int.Parse(userInput);
                 parsResult = int.TryParse(userInput,out userInputInt);
                 if(parsResult == false){
-                    Console.WriteLine("Please Enter a number");
+                    Console.WriteLine("Your Input is not a number. Please Enter a number\n");
                     continue;
                 }
+
                 if(userInputInt > 3 || userInputInt < 1){
-                    Console.WriteLine("Input is out of range, Please enter between 1 -3");
+                    Console.WriteLine("Input is out of range, Please enter between 1 - 3 \n");
                     continue;
                 }
 
               
+
+
             Console.WriteLine("Congratulation you enter : " + userInputInt);
             Random randomNumber = new Random(10);
             int computerChoice = randomNumber.Next(1,4);
             Console.WriteLine("The computer choice is ==> " + computerChoice);
             //check for winner
             if(userInputInt == computerChoice){ // if the players are tied
+
                 Console.WriteLine("This game was a tie");
+
             }else if(userInputInt ==2 && computerChoice == 1){
+
                 Console.WriteLine("Congrats, You (the user) WON!");
             
             }else if(userInputInt == 3 && computerChoice ==2){
+
                 Console.WriteLine("Congrats, You (the user) WON!");
 
             }else if(userInputInt == 1 && computerChoice ==3){
+
                 Console.WriteLine("Congrats, You (the user) WON!");
 
             }else{
+
                 Console.WriteLine("Sorry. The computer WON!");
 
             }
+
 
               Console.WriteLine("Do you want to play again y/n");
               string ch = Console.ReadLine();
