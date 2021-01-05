ad229afa4a23d6f196cdc408f4137ea508aa5b5e
Duangboudda <tongpan78@hotmail.com>
2021-01-05 05:54:09 -0800

    initP1

---
+++ P1_ThongphanhDuangboudda/TrollStore/Models/OrderHistory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+//
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrollStore.Models
+{
+    public class OrderHistory
+    {
+        //TODO
+        [Key]
+        public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer customer { get; set; }
+
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int StoreId { get; set; }
+        public Store store { get; set; }
+
+
+    }
+}
---
+++ P1_ThongphanhDuangboudda/TrollStore/Models/Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TrollStore.Models
+{
+    public class Store
+    {
+        [Key]
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
